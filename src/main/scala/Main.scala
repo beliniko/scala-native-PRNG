4a060026b315076de3f7b63fbd6f7f35bcb78d49
Nikolai Belikov <niko.belikov@icloud.com>
2024-09-11 15:33:48 +0200

    implemented the autocorrelationtest to measure how unpredictale a prng is

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,11 @@
+// to measure how unpradictable a prng is the autocorrelation is used 
 package main.scala
 
 import prng.XORShift
 import prng.LinearCongruentialGenerator
 import prng.MersenneTwister
 import benchmark.EqualDistribution
+import benchmark.unpredictableTest
 object Main {
   def main(args: Array[String]): Unit =
     val randomXORShift = XORShift.seqOfInt(0, 9, 10)
@@ -15,5 +17,8 @@ object Main {
 
     val ChiSquareXORShift = EqualDistribution.calculateChiSquareTest(XORShift.seqOfInt(0, 14, 100000), 15)
     println(s"Chi-Square-Test for XORShift: $ChiSquareXORShift")
+
+    val autocorrelationXORShift = unpredictableTest.autocorrelation(XORShift.seqOfInt(0, 14, 10000), 1)
+    println(s"Autocorrelation for XORShift: $autocorrelationXORShift")
 
 }
